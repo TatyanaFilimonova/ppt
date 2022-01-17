8f6c1d187208bad54dfeb61a2dfd269e16d354d2
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2022-01-17 18:41:29 +0200

    Next step

--- calculateForm.js
+++ calculateForm.js
@@ -1,3 +1,11 @@
+const disclaimer = "<p>"+
+    "<strong>Внимание</strong>: расчет носит иллюстративный характер: он опирается на <strong>базовый</strong> набор элементов. В "+
+    "конкретном проекте набор формируется исходя из потребностей и бюджета.</p>"+
+    "<strong>Важно:</strong> не менее 20% от бюджета проекта - это сумма, которую владелец " +
+    "аналогичного, но не автоматизированого дома потратит на:"+
+    "<ul><li>Клавишные выключатели</li><li>Терморегуляторы</li><li>Многопостовые рамки к электрофурнитуре</li><li>Диммеры</li>"+
+    "<li>Пульты управления</li><li>Фрагментированную в управлении автоматику</li></ul>";
+
 function parseData(formData, formItemName){
     let data = formData.get(formItemName);
     if (data !== null) {
@@ -40,6 +48,7 @@ function clickCalculateButton(formPrefix) {
 
     const floorNumber = parseData(formData, formPrefix+'FloorNumber');
     const square = parseData(formData, formPrefix+'Square');
+    const ceilHeight = parseData(formData, formPrefix+'CeilHeight');
     const bathroomNumber = parseData(formData, formPrefix+'BathroomNumber');
     const roomNumber = parseData(formData, formPrefix+'RoomNumber');
     const spacesNumber = parseData(formData, formPrefix+'SpacesNumber');
@@ -66,20 +75,24 @@ function clickCalculateButton(formPrefix) {
 
     const  componentSet =  {
         leaksSensor: {quantity: bathroomNumber+2, priceMin: 10, priceMax: 15, demand: compPipes, installCost: 5},
-        switchPanel: {quantity: bathroomNumber+roomNumber+parking+spacesNumber, priceMin: 200, priceMax: 400, demand: compIndoorLight, installCost: 10},
-        powerSocket: {quantity: bathroomNumber+roomNumber+parking+spacesNumber+Math.ceil(groundSquare/100), priceMin: 15, priceMax: 150, demand: compIndoorLight, installCost: 5},
-        heatFloor:   {quantity: bathroomNumber+2, priceMin: 15, priceMax: 100, demand: compClimat, installCost: 5},
-        airSensor:   {quantity: roomNumber+bathroomNumber+1, priceMin: 30, priceMax: 100, demand: compClimat, installCost: 5},
-        servoDrive:  {quantity: bathroomNumber, priceMin: 15, priceMax: 150, demand: compPipes, installCost: 10},
-        gateControl: {quantity: parking+3, priceMin: 20, priceMax: 100, demand: compGates, installCost: 15},
+        switchPanel: {quantity: bathroomNumber+roomNumber*2+parking+spacesNumber, priceMin: 150, priceMax: 300, demand: compIndoorLight, installCost: 10},
+        dimmer:      {quantity: Math.ceil(roomNumber/2), priceMin: 290, priceMax: 500, demand: compIndoorLight, installCost: 5},
+        lightActuator: {quantity: Math.ceil((bathroomNumber*2+roomNumber*2+spacesNumber+parking+Math.ceil(groundSquare/200))/6), priceMin: 200, priceMax:300, demand: compIndoorLight, installCost: 5 },
+        socketActuator: {quantity: Math.ceil((bathroomNumber*2+roomNumber*3+spacesNumber+parking*2+Math.ceil(groundSquare/200))/6), priceMin: 200, priceMax: 300, demand: compIndoorLight, installCost: 5},
+        heatFloorActuator:   {quantity: Math.ceil((bathroomNumber+2)/4), priceMin: 150, priceMax: 250, demand: compClimat, installCost: 5},
+        airSensor:   {quantity: (roomNumber+bathroomNumber+1), priceMin: 150, priceMax: 200, demand: compClimat, installCost: 5},
+        climaBridge:   {quantity: (roomNumber+1), priceMin: 300, priceMax: 1000, demand: compClimat, installCost: 10},
+        servoDrive:  {quantity: bathroomNumber+Math.ceil(groundSquare/200)*compIrrigation, priceMin: 50, priceMax: 150, demand: compPipes, installCost: 10},
+        gateControl: {quantity: parking+3, priceMin: 100, priceMax: 200, demand: compGates, installCost: 15},
         mainControl: {quantity: 1, priceMin: 500, priceMax: 1000, demand: 0, installCost: 100},
         irrigator:   {quantity: Math.ceil((groundSquare-square)/200), priceMin: 100, priceMax: 300, demand: compIrrigation, installCost: 15},
         ipPanel:     {quantity: parking+1+floorNumber-1, priceMin: 150, priceMax: 300, demand: compIPtelecom, installCost: 15},
         ipCam:       {quantity: parking+6, priceMin: 100, priceMax: 300, demand: compVideo, installCost: 15},
+        heatControl: {quantity: Math.ceil((bathroomNumber+roomNumber+1+spacesNumber+parking)/6), priceMin: 400, priceMax: 600, demand: compClimat, installCost: 20},
         ipStorage:   {quantity: 1, priceMin: 150, priceMax: 500, demand: compVideo, installCost: 15},
-        curtain:     {quantity: (roomNumber+1)*2, priceMin: 15, priceMax: 50, demand: compCurtains, installCost: 10},
-        wires:       {quantity: square*12, priceMin: 0.8, priceMax: 1.2, demand: taskListWiring, installCost: 0},
-        twistedPair: {quantity: square*5, priceMin: 0.6, priceMax: 1, demand: taskListWiring, installCost: 0}
+        curtain:     {quantity: Math.ceil((roomNumber+1)*2/6), priceMin: 150, priceMax: 50, demand: compCurtains, installCost: 10},
+        wires:       {quantity: Math.ceil(square*12*(1+(2.7+ceilHeight-3)/2.7*0.4)), priceMin: 0.8, priceMax: 1.2, demand: taskListWiring, installCost: 0},
+        twistedPair: {quantity: Math.ceil(square*5*(1+(2.7+ceilHeight-3)/2.7*0.4)), priceMin: 0.6, priceMax: 1, demand: taskListWiring, installCost: 0}
     }
 
     const costs = getMaterialCost(componentSet);
@@ -112,7 +125,10 @@ function clickCalculateButton(formPrefix) {
         wireString+
         electricDevString+
         totalElectro;
-    document.getElementById('HouseAmount').style.display='grid';
+    elementToWrite = document.getElementById(formPrefix+'Disclaimer');
+    elementToWrite.innerHTML = disclaimer;
+    document.getElementById(formPrefix+'Amount').style.display='grid';
+    document.getElementById(formPrefix+'Disclaimer').style.display='grid';
 }
 
 function getMaterialCost(componentSet){
@@ -120,9 +136,6 @@ function getMaterialCost(componentSet){
     let installCost = 0;
     const keys = Object.keys(componentSet);
     keys.map((key) => {
-        console.log(key);
-        console.log(componentSet[key].priceMin*componentSet[key].quantity*componentSet[key].demand)
-
         materialCost+= componentSet[key].priceMin*componentSet[key].quantity*componentSet[key].demand;
         installCost+= componentSet[key].installCost*componentSet[key].quantity*componentSet[key].demand;
     })
