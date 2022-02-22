f716d3784fba4e30a1d46e4760545ff450d88091
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2022-02-22 16:47:26 +0200

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-function SendData(FormName){
-    const form = document.forms[FormName];
-    const formData = new FormData(form);
-    const request = new XMLHttpRequest();
-    request.open("POST", "http://127.0.0.1:8000/calc_api/equipment/");
-    request.send(formData);
-}
