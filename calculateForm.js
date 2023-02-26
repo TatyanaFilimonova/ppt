4814ea1fada4aeb761a1b582ff4de42881b8b998
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2023-02-26 23:02:03 +0200

    Update calculateForm.js

--- calculateForm.js
+++ calculateForm.js
@@ -1,6 +1,6 @@
-const global_url = 'http://127.0.0.1:8000/'
+//const global_url = 'http://127.0.0.1:8000/'
 let phoneNumCounter = 1;
-//let global_url = 'https://pptapi.herokuapp.com/'
+let global_url = 'https://pptapi.herokuapp.com/'
 //let global_language = 'ru'
 
 const disclaimer = "<p>"+
